c22d534b30a67aa558e9aa1162b1955fd843633a
miriaxel <mirm.benmor@gmail.com>
2026-01-22 18:30:22 +0200

    first commit

---
+++ demo.py
@@ -0,0 +1,12 @@
+import random
+
+random_numbers = [random.randint(1,100) for _ in range(10)]
+
+print("Unsorted List:")
+print(random_numbers)
+
+sorted_numbers = sorted(random_numbers)
+
+print("Sorted List:")
+print(sorted_numbers)
+
